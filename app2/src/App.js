16369b3a4b83376d0be2815019f912bb87844e89
NOORULAMEEN JAMAL MOHAMED <noorul.it@gmail.com>
2022-10-12 15:40:23 +0800

    update styled components version

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const App = () => (
-  <ChildAppContainer>
-    <h1>Child App</h1>
-    <h3>React Version {React.version}</h3>
-  </ChildAppContainer>
-);
-
-export const ChildAppContainer = styled.div`
+const ChildAppContainer = styled.div`
   width: calc(100vw - 250px);
   border: solid 1px red;
   height: 900px;
 `;
+
+const App = () => (
+  <ChildAppContainer>
+    <h1>Child App</h1>
+    <h3>React Version {React.version}</h3>
+    <h3>Styled Components Version {styled.version}</h3>
+  </ChildAppContainer>
+);
+
+
 
 export default App;
